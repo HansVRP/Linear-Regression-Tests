d059d628ed504e097bcc45837175ed4b2faeb6e4
HansVRP <hansvanrompay@hotmail.com>
2019-12-15 14:34:53 +0100

    Included NN

--- fromscratch.py
+++ fromscratch.py
@@ -75,4 +75,92 @@ def model(x, w, b):
     return torch.addmm(b,inputs, w.t())
 
 preds = model(inputs, W, B)
-print(preds)
+
+#%% Compare with targets MSE
+
+def MSE(A,B):
+    return torch.mean(torch.pow((A - B), 2))
+
+loss = MSE(preds, targets)
+print(loss)
+
+loss.backward()
+
+#%% Compare with targets MSE
+
+print(w)
+print(w.grad)
+
+print(b)
+print(b.grad)
+
+# after calculating eacht gradient loss, the gradients need to be set to 0
+#pytorch accumulates the loss elsewise
+w.grad.zero_()
+b.grad.zero_()
+print(w.grad)
+print(b.grad)
+
+#%% We can now set up a first training epoch
+
+W = torch.randn(2, 3, requires_grad=True)
+B = torch.randn(2, requires_grad=True)
+# we make an estimate for the output
+preds = model(inputs, W, B)
+
+# we calculate the loss
+loss = MSE(preds, targets)
+print (loss)
+# backpropagate 
+
+loss.backward()
+
+# update W and b
+# we don't want PyTorch to calculate the gradients of the new defined variables w and b
+# since  want to update their values.
+
+lr = 1e-5
+
+with torch.no_grad():
+    W -= W.grad * lr
+    B -= B.grad * lr
+    W.grad.zero_()
+    B.grad.zero_()
+    
+# If a gradient element is negative, increasing the element's value slightly will decrease the loss.
+    
+preds = model(inputs, W, B)
+
+# we calculate the loss
+loss = MSE(preds, targets)
+print (loss)
+
+#%% Remeat the training multiple epochs
+import matplotlib.pyplot as plt
+
+W = torch.randn(2, 3, requires_grad=True)
+B = torch.randn(2, requires_grad=True)
+
+lr = 1e-5
+
+d = []
+
+for i in range(100):
+    
+    preds = model(inputs, W, B)
+    loss = MSE(preds, targets)
+    d.append(loss.data)
+    loss.backward()
+    with torch.no_grad():
+        W -= W.grad * lr
+        B -= B.grad * lr
+        W.grad.zero_()
+        B.grad.zero_()
+        
+fig, ax = plt.subplots()
+ax.plot(d)
+ax.set_xlabel("iteration")
+ax.set_ylabel("distance")
+plt.show()
+
+
